c9be576c5eb5646b8ec43368e50875d4d5c440b5
Cody Herbst <cyherbst@gmail.com>
2016-07-09 23:01:45 -0400

    First commit

---
+++ project_1/src/main.c
@@ -0,0 +1,25 @@
+#include <stm32f401xe.h>
+#include <delay.h>
+ 
+#define LED_PIN 5
+#define LED_TOGGLE() GPIOA->ODR ^= 0x0030;
+ 
+int main() {
+	/* Enbale GPIOA clock */
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+
+	/* Configure GPIOA pin 5 as output */
+	GPIOA->MODER |= (1 << (LED_PIN << 1));
+
+
+	delay_init();
+	
+	while(1){
+		LED_TOGGLE();
+		delay_ms(50);
+	}
+
+	return 0;
+}
+
+
